ee021faa2a4861518a1d6b4e88ad43e452b379e6
RicardoCiotto <ryukyzp@gmail.com>
2025-03-25 23:23:54 -0300

    total update

--- scripts.js
+++ scripts.js
@@ -68,6 +68,17 @@ const SelfServiceMachine = {
     return {
         products:  window.products
     }
+  },
+  methods: {
+    total: function(){
+        var total = 0;
+        this.products.forEach(function(item){
+            if (item.active){
+                total += item.price*item.quantity
+            }
+        });
+        return total.toFixed(2);
+    }
   }
 };
 
